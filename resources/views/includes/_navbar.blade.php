66b47d2df4a8da9ab295668a4a930d712fd3d0c9
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-05-13 15:39:44 -0600

    fixed navbar

--- resources/views/includes/_navbar.blade.php
+++ resources/views/includes/_navbar.blade.php
@@ -2,19 +2,20 @@
     <div class="container">
         <header class="mdl-layout__header">
             <div class="mdl-layout__header-row">
-                <a href="{{ route('home') }}"><span class="mdl-layout-title">Pretty quotes <span class="title-for">for&nbsp;{{ $user ? $user->first_name : 'you' }}</span> </span></a>
+                <a href="{{ route('home') }}"><span class="mdl-layout-title">Pretty quotes <span class="title-for">for&nbsp;{{ $user ? $user->first_name : 'you' }}</span>
+                    </span></a>
                 <div class="row-navigation row-navigation__pages">
-                    <a class="mdl-button navigation navigation-quotes mx-2 {{\Request::is('/') ? 'active' : null}} " href="{{ route('home') }}">Home</a>
+                    <a class="mdl-button navigation navigation-quotes mx-2 {{\Route::current()->getName() === 'home' ? 'active' : null}} " href="{{ route('home') }}">Home</a>
                     @if(Auth::check ())
-                        <a class="mdl-button navigation navigation-create mx-2 {{\Request::is('create' || 'new' || 'edit') ? 'active' : null}}" href="{{ route('create') }}">
-                            Create new
+                        <a class="mdl-button navigation navigation-create mx-2 {{\Route::current()->getName() === 'create' ||\Route::current()->getName() === 'new.get' ||\Route::current()->getName() === 'new.post' ||\Route::current()->getName() ===  'edit.get' || \Route::current()->getName() === 'edit.post' ? 'active' : null }}" href="{{ route('create') }}">
+                            Print shop
                         </a>
                     @endif
                 </div>
                 <div class="row-navigation row-navigation__auth">
                     @guest
-                        <a class="mdl-button navigation navigation-login mx-2 {{\Request::is('login') ? 'active' : null}} " href="{{ route('login') }}">Login</a>
-                        <a class="mdl-button navigation navigation-signup mx-2 {{\Request::is('register') ? 'active' : null}}" href="{{ route('register') }}">Register</a>
+                        <a class="mdl-button navigation navigation-login mx-2 {{Route::current()->getName() === 'login' ? 'active' : null}} " href="{{ route('login') }}">Login</a>
+                        <a class="mdl-button navigation navigation-signup mx-2 {{Route::current()->getName() === 'register' ? 'active' : null}}" href="{{ route('register') }}">Register</a>
                     @else
                         <a class="mdl-button navigation navigation-logout mx-2"
                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
